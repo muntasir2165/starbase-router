4a05567f24b2928da28d240bbb33ba6d5c374592
Muntasir Biojid <muntasir2165@hotmail.com>
2020-06-02 16:40:55 -0700

    v-35 introducing vuerouter

---
+++ src/components/Data.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Type: {{type}}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      type: this.$route.params.type
+    };
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import Vue from "vue";
-import App from "./App.vue";
+import VueRouter from "vue-router";
 
-new Vue({ el: "#app", render: h => h(App) });
+Vue.use(VueRouter);
+
+import App from "./App.vue";
+import Data from "./components/Data.vue";
+
+const routes = [
+  {
+    path: "/data/:type",
+    component: Data
+  }
+];
+
+const router = new VueRouter({ routes });
+
+new Vue({ el: "#app", router, render: h => h(App) });
